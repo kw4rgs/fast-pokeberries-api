6e1226939c15acb6e84e2b97aff3d3877e441d8d
kw4rgs <kw4rgs@gmail.com>
2023-11-25 23:39:21 -0300

    Fix: Tests were improved

--- tests/test_get_all_berry_stats.py
+++ tests/test_get_all_berry_stats.py
@@ -16,24 +16,26 @@ def test_get_all_berry_stats(client):
 def test_get_all_berry_stats_not_found(client):
     response = client.get("/allBerryStats")
     assert response.status_code == 404
-    assert response.json().get('error') == True
     assert response.json().get('detail') == "Not Found"
     
-# Test for Internal Server Error response
+#Test for Internal Server Error response
 def test_get_all_berry_stats_internal_server_error(client, monkeypatch):
     monkeypatch.delenv('POKEBERRIES_API_URL', raising=False)
     response = client.get("api/v1/allBerryStats")
     assert response.status_code == 500
-    assert response.json().get('error') == True
-    assert response.json().get('detail') == "Internal Server Error"
 
-# Test for Timeout response
-def test_get_all_berry_stats_timeout(client, monkeypatch):
-    monkeypatch.setenv('POKEBERRIES_API_URL', 'http://localhost:8000/api/v2/berries')
-    response = client.get("api/v1/allBerryStats")
-    assert response.status_code == 504
-    assert response.json().get('error') == True
-    assert response.json().get('detail') == "Timeout"
+# Test for Method Not Allowed response
+def test_get_all_berry_stats_method_not_allowed(client):
+    response = client.post("/api/v1/allBerryStats")
+    assert response.status_code == 405
+    assert response.json().get('detail') == "Method Not Allowed"
+    
+# Test when the environment variable is missing
+def test_error_handling_missing_url(client, monkeypatch):
+    monkeypatch.delenv('POKEBERRIES_API_URL', raising=False)
+    response = client.get("/api/v1/allBerryStats")
+    assert response.status_code == 500
+    assert response.json().get('detail') == "Missing API URL"
     
 # Test for specific keys in the response
 def test_get_all_items(client):
@@ -51,9 +53,4 @@ def test_get_all_items(client):
     assert 'mean_growth_time' in data
     assert 'frequency_growth_time' in data
 
-# Test when the environment variable is missing
-def test_error_handling_missing_url(client, monkeypatch):
-    monkeypatch.delenv('POKEBERRIES_API_URL', raising=False)
-    response = client.get("/api/v1/allBerryStats")
-    assert response.status_code == 500
-    assert response.json().get('detail') == "Missing API URL"
+
